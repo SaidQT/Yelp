e7b5db3e1eccd5ffe424a308429d2c8a82155460
Muath-Ademar <muazademar20@gmail.com>
2024-05-31 20:41:53 +0300

    improved the design

--- yelp/static/script.js
+++ yelp/static/script.js
@@ -1,48 +1,62 @@
-let nextBtn = document.querySelector('.next')
-let prevBtn = document.querySelector('.prev')
+let nextBtn = document.querySelector('.next');
+let prevBtn = document.querySelector('.prev');
 
-let slider = document.querySelector('.slider')
-let sliderList = slider.querySelector('.slider .list')
-let thumbnail = document.querySelector('.slider .thumbnail')
-let thumbnailItems = thumbnail.querySelectorAll('.item')
+let slider = document.querySelector('.slider');
+let sliderList = slider.querySelector('.slider .list');
+let thumbnail = document.querySelector('.slider .thumbnail');
+let thumbnailItems = thumbnail.querySelectorAll('.item');
 
-thumbnail.appendChild(thumbnailItems[0])
+thumbnail.appendChild(thumbnailItems[0]);
 
 // Function for next button 
 nextBtn.onclick = function () {
-     moveSlider('next')
+     moveSlider('next');
 }
-
 
 // Function for prev button 
 prevBtn.onclick = function () {
-     moveSlider('prev')
+     moveSlider('prev');
 }
 
-
 function moveSlider(direction) {
-     let sliderItems = sliderList.querySelectorAll('.item')
-     let thumbnailItems = document.querySelectorAll('.thumbnail .item')
+     let sliderItems = sliderList.querySelectorAll('.item');
+     let thumbnailItems = document.querySelectorAll('.thumbnail .item');
 
      if (direction === 'next') {
-          sliderList.appendChild(sliderItems[0])
-          thumbnail.appendChild(thumbnailItems[0])
-          slider.classList.add('next')
+          sliderList.appendChild(sliderItems[0]);
+          thumbnail.appendChild(thumbnailItems[0]);
+          slider.classList.add('next');
      } else {
-          sliderList.prepend(sliderItems[sliderItems.length - 1])
-          thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1])
-          slider.classList.add('prev')
+          sliderList.prepend(sliderItems[sliderItems.length - 1]);
+          thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1]);
+          slider.classList.add('prev');
      }
-
 
      slider.addEventListener('animationend', function () {
           if (direction === 'next') {
-               slider.classList.remove('next')
+               slider.classList.remove('next');
           } else {
-               slider.classList.remove('prev')
+               slider.classList.remove('prev');
           }
-     }, { once: true }) // Remove the event listener after it's triggered once
+     }, { once: true }); // Remove the event listener after it's triggered once
 }
+
+// Autoplay functionality
+let autoplayInterval = setInterval(() => {
+     nextBtn.click();
+}, 3000); // Change slide every 3 seconds
+
+// Pause autoplay on mouse hover
+slider.addEventListener('mouseover', () => {
+     clearInterval(autoplayInterval);
+});
+
+// Resume autoplay when not hovering
+slider.addEventListener('mouseout', () => {
+     autoplayInterval = setInterval(() => {
+          nextBtn.click();
+     }, 4000);
+});
 
 window.addEventListener('scroll', function () {
      var header = document.querySelector('header');
